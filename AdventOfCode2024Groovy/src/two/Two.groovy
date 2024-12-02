ea8e1c2c68357066d1a6715cebf1ce25ed0753e3
Miguel Rondon <mrondon008@gmail.com>
2024-12-02 00:40:23 -0500

    Part 2

--- AdventOfCode2024Groovy/src/two/Two.groovy
+++ AdventOfCode2024Groovy/src/two/Two.groovy
@@ -5,22 +5,48 @@ def p1() {
     def a = []
     def b = []
     new File("input.txt").eachLine {
-
+        a << it.split(/\s+/).collect{it as int}
     }
 
-    a.sort()
-    b.sort()
+    r = a.count{s->
+        safe(s)
+    }
 
     println r
 }
 
+def safe(r) {
+    def diff = (1..<r.size()).collect {r[it] - r[it - 1]}
+
+    if (!diff.every {it.abs() in 1..3}) return false
+
+    def inc = diff.every { it > 0 }
+    def dec = diff.every { it < 0 }
+
+    return inc || dec
+}
+
+def safe2(r) {
+    if (safe(r)) return true
+
+    for (int i = 0; i < r.size(); ) {
+        def rr = r[0..<i, ++i..<r.size()]
+        if (safe(rr)) return true
+    }
+
+    return false
+}
 
 def p2() {
-    long r = 0
+    long r = 0;
     def a = []
     def b = []
     new File("input.txt").eachLine {
+        a << it.split(/\s+/).collect{it as int}
+    }
 
+    r = a.count{s->
+        safe2(s)
     }
 
     println r
